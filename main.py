eb7ff5dbd63b5550857aa0ac8e8c277e2b05bfbc
PetraVidnerova <petra.vidnerova@gmail.com>
2016-05-03 10:36:46 +0200

    print errors

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import numpy as np 
 from sklearn.svm import SVC 
 from sklearn.grid_search import GridSearchCV   
-from keras.datasets import mnist
-
+from sklearn.metrics import mean_squared_error, accuracy_score
+import data
 
 def main():
     """ Test SVM from scikit learn on mnist data set.""" 
 
-    (X_train, Y_train), (X_test, Y_test) =  mnist.load_data() 
+    (X_train, Y_train), (X_test, Y_test) =  data.preprocess_mnist() 
   
-    # preprocess data
-    X_train = X_train.reshape(60000, 784)
-    X_test = X_test.reshape(10000, 784)
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
 
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
-
-
-    model = SVC(kernel='rbf') 
-    params = { "C" : np.logspace(-7, 0, 4), 
-               "gamma" : np.logspace(-3, 0, 4) } 
+    model = SVC(kernel='poly', degree=2) 
+    params = { "C" : np.logspace(0, 3, 4), 
+               "gamma" : np.logspace(-7, 2, 4), 
+               "coef0" : np.logspace(-4,4,4)} 
 
     grid = GridSearchCV(model, param_grid = params, 
                         cv=5, n_jobs = 5, pre_dispatch = "n_jobs")
     grid.fit(X_train, Y_train) 
-
     print(grid.best_params_)
+
+    train_yy = grid.predict(X_train)
+    test_yy = grid.predict(X_test) 
+
+    train_err = 100*mean_squared_error(train_yy, Y_train) 
+    test_err = 100*mean_squared_error(test_yy, Y_test) 
+    
+    print("Train. err:", train_err) 
+    print("Test err:", test_err) 
+
+    train_acc = accuracy_score(Y_train, train_yy)  
+    test_acc = accuracy_score(Y_test, test_yy) 
+
+    print("Train. acc:", train_acc) 
+    print("Test acc:", test_acc) 
 
 
 if __name__=="__main__": 
